0f737ef228627ebfb6db90076b268dd2fdbfe560
eby64 <62622232+eby64@users.noreply.github.com>
2020-05-02 02:15:34 +0430

    Add files via upload

---
+++ searcher.py
@@ -0,0 +1,15 @@
+import re ,requests,os
+
+#os.system("clear")
+gitclone="git clone "
+q=input()
+os.system("echo Searching...|lolcat -a -d 5 ")
+q = '+'.join(q.split())+'+github'
+url = 'https://www.google.com/search?q=' + q + '&ie=utf-8&oe=utf-8'
+r = requests.get(url)
+addrss=re.findall(r"<a href=\"/url\?q=(https://github\.com/.+?)&amp;",r.text)
+#print(addrss[0])
+gitclone=gitclone+addrss[0]
+os.system(gitclone)
+#print(addrss[1])
+#print(addrss[2])
